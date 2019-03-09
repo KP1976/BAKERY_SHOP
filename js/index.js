eba413d930b0a1b1272a2e76e726070b9dc55b66
Krzysztof Pawlowski <kpawlowski1976@gmail.com>
2019-03-09 18:22:24 +0100

    install eslint and change menu

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
-console.log('TEST');
+// MENU
+
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+
+const showMenu = e => {
+	console.log(e);
+};
+
+hamburgerMenu.addEventListener('click', showMenu);
