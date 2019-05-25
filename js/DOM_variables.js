991b626821f95a6f62dd46ab6243d1ce934369dd
Krzysztof Pawlowski <kpawlowski1976@gmail.com>
2019-05-25 23:27:07 +0200

    Change some files structure and add products.json

---
+++ js/DOM_variables.js
@@ -0,0 +1,14 @@
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const menu = document.querySelector('.menu-list');
+const menuListItems = document.querySelectorAll('.menu-list__item');
+const phoneNumber = document.querySelector('.menu-list__phone-number');
+const menuProductsListButton = document.querySelector('.products-btn');
+const menuProductsList = document.querySelector('.products-list');
+const menuProducts = document.querySelectorAll('.products-list__product');
+const DOMAmountOfProducts = document.querySelector('.products-btn__quantity');
+const DOMTotalPriceOfProducts = document.querySelector(
+	'.products-btn__total-price',
+);
+const DOMAddProductButtons = document.querySelectorAll(
+	'.product__shop-cart-box',
+);
--- js/index.js
+++ js/index.js
@@ -1,149 +1,128 @@
-// MENU
-const hamburgerMenu = document.querySelector('.hamburger-menu');
-const menu = document.querySelector('.menu-list');
-const menuListItems = document.querySelectorAll('.menu-list__item');
-const phoneNumber = document.querySelector('.menu-list__phone-number');
-const menuProductsListButton = document.querySelector('.products-btn');
-const menuProductsList = document.querySelector('.products-list');
-const menuProducts = document.querySelectorAll('.products-list__product');
+const Main = (_ => {
+	let buttonName = document.querySelector('.products-btn__name');
+	let amountOfProducts = parseInt(DOMAmountOfProducts.textContent);
+	let sumOfAllProducts = 0;
 
-const changeHamburgerMenu = () => {
-	const hamburgerMenuBars = document.querySelectorAll('.hamburger-menu__bar');
-	const [barTop, barMiddle, barBottom] = hamburgerMenuBars;
+	const changeHamburgerMenu = () => {
+		const hamburgerMenuBars = document.querySelectorAll('.hamburger-menu__bar');
+		const [barTop, barMiddle, barBottom] = hamburgerMenuBars;
 
-	barTop.classList.toggle('rotate-right');
-	barMiddle.classList.toggle('rotate-left');
-	barBottom.classList.toggle('hide');
-};
+		barTop.classList.toggle('rotate-right');
+		barMiddle.classList.toggle('rotate-left');
+		barBottom.classList.toggle('hide');
+	};
 
-const showMenu = () => {
-	changeHamburgerMenu();
-	menu.classList.toggle('is-visible');
-	menuProductsList.classList.remove('is-visible');
-	menuProducts.forEach(item => item.classList.remove('show'));
-	menuProductsListButton.classList.remove('open');
-};
+	const showMenu = () => {
+		changeHamburgerMenu();
+		menu.classList.toggle('is-visible');
+		menuProductsList.classList.remove('is-visible');
+		menuProducts.forEach(item => item.classList.remove('show'));
+		menuProductsListButton.classList.remove('open');
+	};
 
-const showProducts = () => {
-	if (window.innerWidth < 768) {
-		menuProductsList.classList.toggle('is-visible');
-		menuProducts.forEach(item => item.classList.toggle('show'));
-		menuProductsListButton.classList.toggle('open');
-	} else {
-		menuProductsList.classList.toggle('is-visible');
-		menuProducts.forEach(item => item.classList.toggle('show'));
+	const showProductsInMenu = () => {
+		if (window.innerWidth < 768) {
+			menuProductsList.classList.toggle('is-visible');
+			menuProducts.forEach(item => item.classList.toggle('show'));
+			menuProductsListButton.classList.toggle('open');
+		} else {
+			menuProductsList.classList.toggle('is-visible');
+			menuProducts.forEach(item => item.classList.toggle('show'));
+		}
+	};
+
+	const showContact = () => {
+		if (window.innerWidth >= 768) {
+			phoneNumber.classList.toggle('show');
+		}
+	};
+
+	// SORTOWANIE PRODUKTÓW
+	const productsTabs = [...document.querySelector('.search-tabs').children];
+	const products = document.querySelectorAll('.product');
+
+	const clearAllProducts = () => {
+		products.forEach(product => {
+			product.style.display = 'none';
+		});
+	};
+
+	function showSelectedProducts() {
+		productsTabs.forEach(productTab => {
+			productTab.className = 'search-tabs__item';
+		});
+
+		clearAllProducts();
+
+		products.forEach((product, index) => {
+			if (product.getAttribute('data-category') === this.textContent) {
+				products[index].style.display = 'block';
+			}
+
+			if (this.textContent === 'Wszystko') {
+				products[index].style.display = 'block';
+			}
+		});
+
+		this.className = 'search-tabs__item selected';
 	}
-};
 
-const showContact = () => {
-	if (window.innerWidth >= 768) {
-		phoneNumber.classList.toggle('show');
-	}
-};
-
-hamburgerMenu.addEventListener('click', showMenu);
-menuListItems[3].addEventListener('click', showContact);
-
-// SORTOWANIE PRODUKTÓW
-const productsTabs = [...document.querySelector('.search-tabs').children];
-const products = document.querySelectorAll('.product');
-
-const clearAllProducts = () => {
-	products.forEach(product => {
-		product.style.display = 'none';
-	});
-};
-
-function showSelectedProducts() {
 	productsTabs.forEach(productTab => {
-		productTab.className = 'search-tabs__item';
+		productTab.addEventListener('click', showSelectedProducts);
 	});
 
-	clearAllProducts();
+	function addProduct(e) {
+		let productPrice = parseFloat(
+			e.target.nextElementSibling.lastElementChild.textContent
+				.split(' ')[0]
+				.replace(',', '.'),
+		);
+		let productPictureSource = e.target.parentElement.firstElementChild.getAttribute(
+			'src',
+		);
+		let productPictureAlt = e.target.parentElement.firstElementChild.getAttribute(
+			'alt',
+		);
+		let productName = e.target.nextElementSibling.firstElementChild.textContent;
+		let amountOfProduct = e.target.previousElementSibling.textContent;
 
-	products.forEach((product, index) => {
-		if (product.getAttribute('data-category') === this.textContent) {
-			products[index].style.display = 'block';
+		amountOfProduct++;
+		e.target.previousElementSibling.textContent = amountOfProduct;
+
+		if (amountOfProduct === 1) {
+			e.target.previousElementSibling.classList.add('visible');
 		}
 
-		if (this.textContent === 'Wszystko') {
-			products[index].style.display = 'block';
+		menuProductsListButton.addEventListener('click', showProductsInMenu);
+
+		amountOfProducts++;
+		sumOfAllProducts += productPrice;
+
+		// Zamiana odpowiedniego słowa w zależności od ilości produktów
+		if (amountOfProducts === 0 || amountOfProducts === 5) {
+			buttonName.textContent = 'produktów – ';
+		} else if (amountOfProducts === 1) {
+			buttonName.textContent = 'produkt – ';
+		} else if (amountOfProducts === 2) {
+			buttonName.textContent = 'produkty – ';
 		}
-	});
 
-	this.className = 'search-tabs__item selected';
-}
+		// Zamiana . na , i na odwrót w cenie produktu
+		if (sumOfAllProducts.toString().includes('.')) {
+			sumOfAllProducts = sumOfAllProducts.toString().replace('.', ',');
+			DOMTotalPriceOfProducts.textContent = sumOfAllProducts + ' zł';
+			sumOfAllProducts = sumOfAllProducts.toString().replace(',', '.');
+			sumOfAllProducts = parseFloat(sumOfAllProducts);
+		} else {
+			DOMTotalPriceOfProducts.textContent = sumOfAllProducts.toString() + ' zł';
+		}
 
-productsTabs.forEach(productTab => {
-	productTab.addEventListener('click', showSelectedProducts);
-});
+		DOMAmountOfProducts.textContent = amountOfProducts;
 
-// OBSŁUGA DODAWANIA I KASOWANIA PRODUKTÓW
-const DOMAmountOfProducts = document.querySelector('.products-btn__quantity');
-const DOMTotalPriceOfProducts = document.querySelector(
-	'.products-btn__total-price',
-);
-const DOMAddProductButtons = document.querySelectorAll(
-	'.product__shop-cart-box',
-);
+		const li = document.createElement('li');
 
-let buttonName = document.querySelector('.products-btn__name');
-
-let amountOfProducts = parseInt(DOMAmountOfProducts.textContent);
-let sumOfAllProducts = 0;
-
-function addProduct(e) {
-	let productPrice = parseFloat(
-		e.target.nextElementSibling.lastElementChild.textContent
-			.split(' ')[0]
-			.replace(',', '.'),
-	);
-	let productPictureSource = e.target.parentElement.firstElementChild.getAttribute(
-		'src',
-	);
-	let productPictureAlt = e.target.parentElement.firstElementChild.getAttribute(
-		'alt',
-	);
-	let productName = e.target.nextElementSibling.firstElementChild.textContent;
-	let amountOfProduct = e.target.previousElementSibling.textContent;
-
-	amountOfProduct++;
-	e.target.previousElementSibling.textContent = amountOfProduct;
-
-	if (amountOfProduct === 1) {
-		e.target.previousElementSibling.classList.add('visible');
-	}
-
-	menuProductsListButton.addEventListener('click', showProducts);
-
-	amountOfProducts++;
-	sumOfAllProducts += productPrice;
-
-	// Zamiana odpowiedniego słowa w zależności od ilości produktów
-	if (amountOfProducts === 0 || amountOfProducts === 5) {
-		buttonName.textContent = 'produktów – ';
-	} else if (amountOfProducts === 1) {
-		buttonName.textContent = 'produkt – ';
-	} else if (amountOfProducts === 2) {
-		buttonName.textContent = 'produkty – ';
-	}
-
-	// Zamiana . na , i na odwrót w cenie produktu
-	if (sumOfAllProducts.toString().includes('.')) {
-		sumOfAllProducts = sumOfAllProducts.toString().replace('.', ',');
-		DOMTotalPriceOfProducts.textContent = sumOfAllProducts + ' zł';
-		sumOfAllProducts = sumOfAllProducts.toString().replace(',', '.');
-		sumOfAllProducts = parseFloat(sumOfAllProducts);
-	} else {
-		DOMTotalPriceOfProducts.textContent = sumOfAllProducts.toString() + ' zł';
-	}
-
-	DOMAmountOfProducts.textContent = amountOfProducts;
-
-	const li = document.createElement('li');
-
-	li.className = 'products-list__product show';
-	li.innerHTML = `
+		li.className = 'products-list__product show';
+		li.innerHTML = `
 	<img
 			src="${productPictureSource}"
 			alt="${productPictureAlt}"
@@ -158,11 +137,44 @@ function addProduct(e) {
 		>
 	`;
 
-	document
-		.querySelector('.products-list__clear-button')
-		.insertAdjacentElement('beforebegin', li);
-}
+		document
+			.querySelector('.products-list__clear-button')
+			.insertAdjacentElement('beforebegin', li);
+	}
 
-DOMAddProductButtons.forEach(productButton => {
-	productButton.addEventListener('click', addProduct);
-});
+	// Fetch JSON data
+	const getProductsFromJSON = async () => {
+		try {
+			const result = await fetch('../products.json');
+			let data = await result.json();
+
+			data = data.map(product => {
+				const { id, name, category } = product;
+				return { id, name, category };
+			});
+			return data;
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	const executeEventListeners = () => {
+		hamburgerMenu.addEventListener('click', showMenu);
+		menuListItems[3].addEventListener('click', showContact);
+		DOMAddProductButtons.forEach(productButton => {
+			productButton.addEventListener('click', addProduct);
+		});
+	};
+
+	return {
+		init: () => {
+			executeEventListeners();
+			getProductsFromJSON().then(products =>
+				products.forEach(product => {
+					console.log(product.id, product.name, product.category);
+				}),
+			);
+		},
+	};
+})();
+document.addEventListener('DOMContentLoaded', Main.init());
