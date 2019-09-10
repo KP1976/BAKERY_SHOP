12d7f9b9752aca820a44f6c29402fcc97a51e342
Krzysztof Pawlowski <kpawlowski1976@gmail.com>
2019-09-10 20:46:05 +0200

    Update firebase.js

--- src/DataBase/firebase.js
+++ src/DataBase/firebase.js
@@ -1,14 +1,16 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 
-firebase.initializeApp({
-	apiKey: 'AIzaSyDGAyftLCspEF-3gm0RJ1-3QlqIx6Dfg4o',
-	authDomain: 'bakery-shop-5457e.firebaseapp.com',
-	databaseURL: 'https://bakery-shop-5457e.firebaseio.com',
+const config = {
+	apiKey: process.env.REACT_APP_FIREBASE_KEY,
+	authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+	databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
 	projectId: 'bakery-shop-5457e',
-	storageBucket: 'bakery-shop-5457e.appspot.com',
-	messagingSenderId: '1009026750919',
-	appId: '1:1009026750919:web:798e2def54d149d2',
-});
+	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+	appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
+
+firebase.initializeApp(config);
 
 export default firebase;
