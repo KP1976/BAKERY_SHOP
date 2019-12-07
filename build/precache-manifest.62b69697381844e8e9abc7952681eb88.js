1141f1107cdd907c5dc7372006c1e07a931a7efc
Krzysztof Pawlowski <kpawlowski1976@gmail.com>
2019-12-07 15:47:08 +0100

    Build React Project

---
+++ build/precache-manifest.62b69697381844e8e9abc7952681eb88.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e9bc4f9af4d0a2c73eac372b3c93c14",
+    "url": "/BAKERY_SHOP/index.html"
+  },
+  {
+    "revision": "aa92990016eb9fd3c259",
+    "url": "/BAKERY_SHOP/static/css/main.87cb14b7.chunk.css"
+  },
+  {
+    "revision": "ebf229200d17e8d0e734",
+    "url": "/BAKERY_SHOP/static/js/2.8645b4e4.chunk.js"
+  },
+  {
+    "revision": "aa92990016eb9fd3c259",
+    "url": "/BAKERY_SHOP/static/js/main.71ddf14a.chunk.js"
+  },
+  {
+    "revision": "27745dd64db2eee05f9b",
+    "url": "/BAKERY_SHOP/static/js/runtime-main.5490e787.js"
+  },
+  {
+    "revision": "02924d4ea5a3e17dd3d8dcce5b3ef426",
+    "url": "/BAKERY_SHOP/static/media/1_citrus_cupcake.02924d4e.jpg"
+  },
+  {
+    "revision": "8e81ab635db3b170a5fb822ae4425476",
+    "url": "/BAKERY_SHOP/static/media/1_citrus_cupcake_small.8e81ab63.jpg"
+  },
+  {
+    "revision": "ccae17e8d233bb1f6021666b0ad432d5",
+    "url": "/BAKERY_SHOP/static/media/2_cheese_cake.ccae17e8.jpg"
+  },
+  {
+    "revision": "2285b8d3bcd9f8d71bfc9ffb83f06837",
+    "url": "/BAKERY_SHOP/static/media/2_cheese_cake_small.2285b8d3.jpg"
+  },
+  {
+    "revision": "c61f4ee682132d630a8d31adc0e34947",
+    "url": "/BAKERY_SHOP/static/media/3_fruit_cupcake.c61f4ee6.jpg"
+  },
+  {
+    "revision": "80e782bb3b0be1bf69bc9364c0a84b04",
+    "url": "/BAKERY_SHOP/static/media/3_fruit_cupcake_small.80e782bb.jpg"
+  },
+  {
+    "revision": "d019ce7843d64f281a88db8b6a8ed20b",
+    "url": "/BAKERY_SHOP/static/media/4_citric_candy.d019ce78.jpg"
+  },
+  {
+    "revision": "860dee9abd373f7a1c8278835a9a73ad",
+    "url": "/BAKERY_SHOP/static/media/4_citric_candy_small.860dee9a.jpg"
+  },
+  {
+    "revision": "f382daeb23850c574d5ab8e9267abbd3",
+    "url": "/BAKERY_SHOP/static/media/5_meringue.f382daeb.jpg"
+  },
+  {
+    "revision": "8931c8655cdd1f6c9c4fda1283f0324c",
+    "url": "/BAKERY_SHOP/static/media/5_meringue_small.8931c865.jpg"
+  },
+  {
+    "revision": "b0293ddd40d035dd08fe418012d246c5",
+    "url": "/BAKERY_SHOP/static/media/6_raspberry_candy.b0293ddd.jpg"
+  },
+  {
+    "revision": "44abbb7c86e2a55520967f1c1bed09f7",
+    "url": "/BAKERY_SHOP/static/media/6_raspberry_candy_small.44abbb7c.jpg"
+  },
+  {
+    "revision": "b7c7cbb924bdcb9d77691583636bed1e",
+    "url": "/BAKERY_SHOP/static/media/7_cake.b7c7cbb9.jpg"
+  },
+  {
+    "revision": "49828c0d681c926fe2915b34c8b5188f",
+    "url": "/BAKERY_SHOP/static/media/7_cake_small.49828c0d.jpg"
+  },
+  {
+    "revision": "aaaa50474f1980e6aca1dd9de1d75eeb",
+    "url": "/BAKERY_SHOP/static/media/8_opium_cake.aaaa5047.jpg"
+  },
+  {
+    "revision": "9ff1547fabc267682284c93bdf12bd8f",
+    "url": "/BAKERY_SHOP/static/media/8_opium_cake_small.9ff1547f.jpg"
+  },
+  {
+    "revision": "88b34159f4695d03e80849addc9aa823",
+    "url": "/BAKERY_SHOP/static/media/9_donut1.88b34159.jpg"
+  },
+  {
+    "revision": "8aa7ede2b09806116e08c53077a50248",
+    "url": "/BAKERY_SHOP/static/media/9_donut1_small.8aa7ede2.jpg"
+  },
+  {
+    "revision": "e69abf8ee93bb2d4e674a7c79ef3c269",
+    "url": "/BAKERY_SHOP/static/media/about_us_picture.e69abf8e.jpg"
+  },
+  {
+    "revision": "4827ba500e7d7484c94b52a13de59495",
+    "url": "/BAKERY_SHOP/static/media/about_us_picture_small.4827ba50.jpg"
+  },
+  {
+    "revision": "9a21d75da034a7afc29b248e6a28f305",
+    "url": "/BAKERY_SHOP/static/media/header-photo.9a21d75d.jpg"
+  },
+  {
+    "revision": "447212a480e1b60f6f03d38c161ef890",
+    "url": "/BAKERY_SHOP/static/media/header-photo_small.447212a4.jpg"
+  }
+]);
